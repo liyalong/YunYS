4ed6c13a37abc110359428630adf4f6bfeaf7600
Streamduo <2314237525@qq.com>
2018-01-16 20:54:52 +0800

    byte数组转文件

--- app/src/main/java/com/yunyisheng/app/yunys/utils/CommonUtils.java
+++ app/src/main/java/com/yunyisheng/app/yunys/utils/CommonUtils.java
@@ -23,9 +23,12 @@ import android.view.WindowManager;
 
 import com.yunyisheng.app.yunys.utils.encrypt.MD5;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.text.DateFormat;
@@ -337,6 +340,59 @@ public class CommonUtils {
             e.printStackTrace();
         }
         return des;
+    }
+
+    /**
+     * 根据byte数组生成文件
+     *
+     * @param bytes
+     *            生成文件用到的byte数组
+     */
+    private void createFileWithByte(byte[] bytes) {
+        /**
+         * 创建File对象，其中包含文件所在的目录以及文件的命名
+         */
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "123.jpg");
+        // 创建FileOutputStream对象
+        FileOutputStream outputStream = null;
+        // 创建BufferedOutputStream对象
+        BufferedOutputStream bufferedOutputStream = null;
+        try {
+            // 如果文件存在则删除
+            if (file.exists()) {
+                file.delete();
+            }
+            // 在文件系统中根据路径创建一个新的空文件
+            file.createNewFile();
+            // 获取FileOutputStream对象
+            outputStream = new FileOutputStream(file);
+            // 获取BufferedOutputStream对象
+            bufferedOutputStream = new BufferedOutputStream(outputStream);
+            // 往文件所在的缓冲输出流中写byte数据
+            bufferedOutputStream.write(bytes);
+            // 刷出缓冲输出流，该步很关键，要是不执行flush()方法，那么文件的内容是空的。
+            bufferedOutputStream.flush();
+        } catch (Exception e) {
+            // 打印异常信息
+            e.printStackTrace();
+        } finally {
+            // 关闭创建的流对象
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bufferedOutputStream != null) {
+                try {
+                    bufferedOutputStream.close();
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
     }
 
     /**
