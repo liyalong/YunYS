b56d8217aacadb2051624ec60cb8acce17dd49c0
Streamduo <2314237525@qq.com>
2018-01-18 17:29:04 +0800

    修改任务表单ui

---
+++ app/src/main/java/com/yunyisheng/app/yunys/main/present/HomePresent.java
@@ -0,0 +1,14 @@
+package com.yunyisheng.app.yunys.main.present;
+
+import com.yunyisheng.app.yunys.main.fragement.HomeFragement;
+
+import cn.droidlover.xdroidmvp.mvp.XPresent;
+
+/**
+ * 作者：fuduo on 2018/1/17 10:22
+ * 邮箱：duoendeavor@163.com
+ * 用途：
+ */
+
+public class HomePresent extends XPresent<HomeFragement> {
+}
---
+++ app/src/main/java/com/yunyisheng/app/yunys/utils/MyListView.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yunyisheng.app.yunys.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * 作者：MarkMingShuai
+ * 时间 2017-8-9 13:39
+ * 邮箱：mark_mingshuai@163.com
+ * 类的意图：自定义ListView
+ */
+public class MyListView extends ListView {
+
+	public MyListView(Context context) {
+		super(context);
+	}
+
+	public MyListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, expandSpec);
+	}
+
+
+}
