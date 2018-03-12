4035410c9fddf9af3bf071c92a7ea788e6f32f14
Streamduo <2314237525@qq.com>
2018-03-12 11:28:39 +0800

    上传图片

---
+++ app/src/main/java/com/yunyisheng/app/yunys/project/bean/UploadDynamicFormImageBean.java
@@ -0,0 +1,26 @@
+package com.yunyisheng.app.yunys.project.bean;
+
+import com.yunyisheng.app.yunys.base.BaseModel;
+
+/**
+ * 作者：fuduo on 2018/3/12 10:41
+ * 邮箱：duoendeavor@163.com
+ * 用途：
+ */
+
+public class UploadDynamicFormImageBean extends BaseModel {
+
+    /**
+     * respBody : /dfgdsgsdfgadfg/sdfdasdds/dfdsfdsf.jpg
+     */
+
+    private String respBody;
+
+    public String getRespBody() {
+        return respBody;
+    }
+
+    public void setRespBody(String respBody) {
+        this.respBody = respBody;
+    }
+}
