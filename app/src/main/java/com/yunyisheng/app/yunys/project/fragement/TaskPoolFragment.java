3d51a92d794523fc2a45430b6fe48030a2aabb74
liyalong <415157877@qq.com>
2018-03-14 14:10:13 +0800

    任务池列表增加event事件刷新列表

--- app/src/main/java/com/yunyisheng/app/yunys/project/fragement/TaskPoolFragment.java
+++ app/src/main/java/com/yunyisheng/app/yunys/project/fragement/TaskPoolFragment.java
@@ -31,6 +31,7 @@ import com.yunyisheng.app.yunys.project.activity.RenwuFankuiFormActivity;
 import com.yunyisheng.app.yunys.project.activity.TaskDetailActivity;
 import com.yunyisheng.app.yunys.project.adapter.SpinnerAdapter;
 import com.yunyisheng.app.yunys.project.model.TaskListModel;
+import com.yunyisheng.app.yunys.project.model.TaskMessageEvent;
 import com.yunyisheng.app.yunys.tasks.activity.CreateDeviceTaskAcitvity;
 import com.yunyisheng.app.yunys.tasks.activity.MyPushTaskChildListActivity;
 import com.yunyisheng.app.yunys.tasks.activity.SelectProjectUserListActivity;
@@ -42,6 +43,10 @@ import com.yunyisheng.app.yunys.utils.DateTimeDialogUtils;
 import com.yunyisheng.app.yunys.utils.ScreenUtils;
 import com.yunyisheng.app.yunys.utils.ScrowUtil;
 import com.yunyisheng.app.yunys.utils.ToastUtils;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -532,5 +537,19 @@ public class TaskPoolFragment extends BaseFragement<TaskListPresent> implements 
     public void showList(){
         noDataTask.setVisibility(View.GONE);
         taskListView.setVisibility(View.VISIBLE);
+    }
+    //订阅方法，当接收到事件的时候，会调用该方法
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(TaskMessageEvent messageEvent){
+        String position = messageEvent.getPosition();
+        if (position.equals("updateOK")){
+            getP().getTaskList(SELECT_TYPE, projectId, PAGE_NUM, PAGE_SIZE);
+        }
+
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
